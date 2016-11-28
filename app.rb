0caf57e7a1e578eb46a0c52248f8d226def3ff2c
Christopher Sanchez <sanchez.christopherm@gmail.com>
2016-11-28 16:56:03 +0100

    Appsy done

--- app.rb
+++ app.rb
@@ -1,16 +1,26 @@
 require 'sinatra/base'
 
 class Appsy < Sinatra::Base
+
+  data_store = {}
+
+
   get '/' do
-    erb :index
+    'Hello from Andy!'
   end
 
-  get '/names' do
-    @partner1 = params[:partner_1_name]
-    @partner2 = params[:partner_2_name]
-    erb :match
+
+  get '/set' do
+    data_store.merge!(params)
+    'Data Stored!'
+  end
+
+  get '/get' do
+    data_store[params[:key]]
   end
 
   # start the server if ruby file executed directly
   run! if app_file == $0
 end
+
+#learned this from a quick work shop with mr. Jenkins
